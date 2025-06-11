2113bbf3aacaf4c7d5fa7bb912a789948513f57d
Haruyuki Tanukiji <haruyuki@tanu.ch>
2025-06-11 15:55:00 +0800

    Forgot to export ROM interfaces

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export * from './kumoha-engine.js';
 
 export * from './types/console/plugin-meta.js';
 export * from './types/opentetsu-additions/game-state.js';
+export * from './types/rom/rollingstock-rom.js';
+export * from './types/rom/route-rom.js';
